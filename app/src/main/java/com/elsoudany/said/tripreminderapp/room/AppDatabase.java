a7d7ac2a2c688b1306c980418f90919545770807
SaidElsoudany <62873010+SaidElsoudany@users.noreply.github.com>
2021-03-26 16:50:36 +0200

    Database Changed

--- app/src/main/java/com/elsoudany/said/tripreminderapp/room/AppDatabase.java
+++ app/src/main/java/com/elsoudany/said/tripreminderapp/room/AppDatabase.java
@@ -4,10 +4,14 @@ import androidx.room.Database;
 import androidx.room.RoomDatabase;
 
 
-@Database(entities = {Trip.class, User.class}, version = 1,exportSchema = false)
+@Database(entities = {Trip.class, User.class,Note.class}, version = 1,exportSchema = false)
 public abstract class AppDatabase extends RoomDatabase {
-    public abstract TripDAO tripDAO();
-    public abstract UserTripDAO userTripDAO();
     public abstract UserDAO userDAO();
+    public abstract UserTripDAO userTripDAO();
+    public abstract TripDAO tripDAO();
+    public abstract TripNoteDao tripNoteDao();
+    public abstract NoteDao noteDao();
+
+
 
 }
